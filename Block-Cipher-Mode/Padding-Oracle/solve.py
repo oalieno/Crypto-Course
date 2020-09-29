5f620efeca906952537ae9f02318a081f0f24a20
OAlienO <jeffrey6910@gmail.com>
2020-09-29 06:57:21 +0000

    update Padding-Oracle solve.py

--- Block-Cipher-Mode/Padding-Oracle/solve.py
+++ Block-Cipher-Mode/Padding-Oracle/solve.py
@@ -21,6 +21,9 @@ for i in range(16, len(enc), 16):
     iv, block = enc[i-16:i], enc[i:i+16]
     for j in range(16):
         for k in range(256):
+            # if you change nothing, padding will be correct, but not the case we want
+            if j == 0 and k == iv[15]:
+                continue
             if oracle(iv[:16 - 1 - j] + bytes([k]) + xor(bytes([j + 1] * j), xor(iv[-j:], ans)) + block):
                 ans = bytes([iv[16 - 1 - j] ^ k ^ (j + 1)]) + ans
                 print(ans)
